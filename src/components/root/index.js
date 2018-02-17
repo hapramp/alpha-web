f09dc86bc44735ca7974bd9dde7d77d512eaf585
Pratyush Singh <singh.pratyush96@gmail.com>
2018-02-17 14:03:08 +0530

    Add root component

---
+++ src/components/root/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Root extends React.Component {
+  render() {
+    return <div>
+      This is root!
+    </div>
+  }
+}
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {compose, createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 import thunk from 'redux-thunk';
+import Icons from 'uikit/dist/js/uikit-icons';
+import UIKit from 'uikit';
 
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
-
-// UIKit Icons
-import Icons from 'uikit/dist/js/uikit-icons';
-import UIKit from 'uikit';
+import Root from './components/root';
 
 UIKit.use(Icons);
 
@@ -16,5 +17,11 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-ReactDOM.render(<div>Hello</div>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
