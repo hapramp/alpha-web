0454bebc4cdd5f0e8d0917f94610f5539e823a42
Pratyush Singh <singh.pratyush96@gmail.com>
2018-02-21 13:28:13 +0530

    Improve add content button

--- src/components/addContentButton/index.js
+++ src/components/addContentButton/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom';
+import {withRouter, Link} from 'react-router-dom';
 
 import styles from './styles.scss';
 import indexStyles from '../../index.scss';
@@ -8,16 +8,18 @@ import {toggleClicked} from "../../actions/addContentActions";
 
 class AddContentButton extends React.Component {
 	render() {
+		let rotation = this.props.isClicked ? 'rotate(135deg)' : '';
 		return <div className={['uk-align-right', 'uk-margin-right', 'uk-margin-bottom', 'uk-text-center',
 			styles.addContent].join(' ')}>
-
 			{this.props.isClicked &&
 			<div className={[indexStyles.transition].join(' ')}>
-				<div className={['uk-margin-bottom', styles.contentType].join(' ')}>Article</div>
-				<div className={['uk-margin-bottom', styles.contentType].join(' ')}>Post</div>
+				<div className={['uk-margin-bottom', styles.contentType].join(' ')}>
+					<Link to={'/create/article'}>Article</Link></div>
+				<div className={['uk-margin-bottom', styles.contentType].join(' ')}>
+					<Link to={'/create/post'}>Post</Link></div>
 			</div>}
 
-			<span uk-icon="icon: plus" className={[styles.addButton, indexStyles.hoverEffect,
+			<span uk-icon="icon: plus" style={{transform: rotation}} className={[styles.addButton,
 				indexStyles.transition].join(' ')} onClick={this.props.toggleClicked}/>
 		</div>
 	}
