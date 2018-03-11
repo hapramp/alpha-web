a76b67201d37e423a219590700b4e5ca2be0b6a5
Pratyush Singh <singh.pratyush96@gmail.com>
2018-03-11 17:53:15 +0530

    Init firebase app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import Icons from 'uikit/dist/js/uikit-icons';
 import UIKit from 'uikit';
+import * as firebase from 'firebase';
 
 import registerServiceWorker from './registerServiceWorker';
 import Root from './components/root';
@@ -12,6 +13,16 @@ import getStore from './utils/storeUtils';
 UIKit.use(Icons);
 
 const store = getStore();
+
+let config = {
+	apiKey: "AIzaSyBVUoUB41eL2GS_ERrG5bAfrjr1bukCu2g",
+	authDomain: "hapramp-625c8.firebaseapp.com",
+	databaseURL: "https://hapramp-625c8.firebaseio.com",
+	projectId: "hapramp-625c8",
+	storageBucket: "hapramp-625c8.appspot.com",
+	messagingSenderId: "574002232827"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
 	<Provider store={store}>
