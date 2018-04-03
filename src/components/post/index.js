6a5c5a5b304c4c52e362aa4e4aab09345694362a
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-03 19:47:33 +0530

    Revert to react-time-ago

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import _ from 'lodash';
-import TimeAgo from 'timeago-react'
+import TimeAgo from 'react-time-ago'
 
 import userPlaceholder from '../userProfile/user-placeholder.jpg';
 import PostData from '../postData';
@@ -160,7 +160,7 @@ class Post extends React.Component {
 			<div className={['uk-flex', styles.paddingModerate, styles.topSection].join(' ')}>
 				<img src={user.json_metadata.profile.profile_image} className={['uk-border-circle', styles.userImage].join(' ')} alt={""}/>
 				<div className={['uk-margin-left'].join(' ')}>
-					<div className={[styles.userNameContainer].join(' ')}><span className={styles.userName}>{user.json_metadata.profile.name}</span> | <TimeAgo datetime={this.props.post.created + 'Z'} locale='en_US'/></div>
+					<div className={[styles.userNameContainer].join(' ')}><span className={styles.userName}>{user.json_metadata.profile.name}</span> | <TimeAgo>{new Date(this.props.post.created + 'Z')}</TimeAgo></div>
 					<div>{communities.map((community, idx) => <span key={idx} style={{backgroundColor: community.color}}
 							className={[styles.communityLabel].join(' ')}>
 							{community.name}
--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ import registerServiceWorker from './registerServiceWorker';
 import Root from './components/root';
 import getStore from './utils/storeUtils';
 
+import TimeAgo from 'javascript-time-ago'
+
+// Load locale-specific relative date/time formatting rules.
+import en from 'javascript-time-ago/locale/en'
+
+// Add locale-specific relative date/time formatting rules.
+TimeAgo.locale(en)
+
 UIKit.use(Icons);
 
 const store = getStore();
