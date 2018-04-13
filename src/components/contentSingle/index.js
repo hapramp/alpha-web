e66b1dde517c95f31518231e910bf4fab74c9391
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-13 16:35:21 +0530

    Fetch post from store in single view

--- src/components/contentSingle/index.js
+++ src/components/contentSingle/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 class ContentSingle extends React.Component {
+	constructor(props) {
+		super(props);
+		// TODO: Load post using actions
+	}
+
 	render() {
-		return <div>
+		return <div className={['uk-container', 'uk-margin-top'].join(' ')}>
 			This is full content for the post.
 		</div>
 	}
 }
 
-export default connect()(ContentSingle);
+const mapStateToProps = (state, ownProps) => {
+	return {
+		post: state.allPosts.posts[ownProps.match.params.username + '/' + ownProps.match.params.permlink]
+	}
+}
+
+export default connect(mapStateToProps)(ContentSingle);
