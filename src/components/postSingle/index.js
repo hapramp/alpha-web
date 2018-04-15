7516b040bbcda960473b399879c8728a7dc93e30
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-15 13:52:55 +0530

    Improve image display and fix style issues in single post view

--- src/components/postSingle/index.js
+++ src/components/postSingle/index.js
@@ -5,13 +5,14 @@ import indexStyles from '../../index.scss';
 import PostUserMeta from '../postUserMeta';
 import {getCommunitiesForPost} from '../../utils/communityUtils';
 import {fixUser} from '../../utils/defaultFixUtils';
+import styles from './styles.scss';
+import PostData from '../postData';
 
 class PostSingle extends React.Component {
 	getLeftSection() {
-
 		if (this.props.post.json_metadata.content.data[0].type === 'image') {
 			return	<div className={['uk-width-1-1@s', 'uk-width-1-2@m', 'uk-width-1-2@l', 'uk-padding-remove'].join(' ')}>
-				<img src={this.props.post.json_metadata.content.data[0].content} style={{margin: 'auto'}} alt=''/>
+				<PostData data={this.props.post.json_metadata.content.data[0]}/>
 			</div>
 		}
 	}
@@ -21,10 +22,12 @@ class PostSingle extends React.Component {
 		let classes = ['uk-width-1-1@s', 'uk-width-2-3@m', 'uk-width-1-3@l'];
 		containsImage && (classes = ['uk-width-1-1@s', 'uk-width-1-2@m', 'uk-width-1-2@l'])
 		let data = containsImage ? this.props.post.json_metadata.content.data[1] : this.props.post.json_metadata.content.data[1]
-		return <div className={classes.join(' ')}>
-			<PostUserMeta profile={{name: this.props.postingUser.json_metadata.profile.name, image: this.props.postingUser.json_metadata.profile.image}}
-				created={this.props.post.created} communities={getCommunitiesForPost(this.props.post)}/>
-			{data.content}
+		return <div className={classes.concat([]).join(' ')}>
+			<PostUserMeta profile={{name: this.props.postingUser.json_metadata.profile.name, image: this.props.postingUser.json_metadata.profile.profile_image}}
+				created={this.props.post.created} communities={getCommunitiesForPost(this.props.post)} className={['uk-padding-remove-left'].join(' ')}/>
+			<div className={[styles.rightContent].join(' ')}>
+				{data.content}
+			</div>
 		</div>
 	}
 
