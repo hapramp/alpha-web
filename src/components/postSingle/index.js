baee44e5ffe9fe8c8f5bc8db0b6856027c6d6ab1
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-15 22:56:24 +0530

    Add custom tags on single post

--- src/components/postSingle/index.js
+++ src/components/postSingle/index.js
@@ -8,6 +8,7 @@ import {fixUser} from '../../utils/defaultFixUtils';
 import styles from './styles.scss';
 import PostData from '../postData';
 import ActionBar from '../actionBar';
+import CustomTags from '../customTags';
 
 class PostSingle extends React.Component {
 	getLeftSection() {
@@ -22,13 +23,14 @@ class PostSingle extends React.Component {
 		let containsImage = this.props.post.json_metadata.content.data[0].type === 'image';
 		let classes = ['uk-width-1-1@s', 'uk-width-2-3@m', 'uk-width-1-3@l'];
 		containsImage && (classes = ['uk-width-1-1@s', 'uk-width-1-2@m', 'uk-width-1-2@l'])
-		let data = containsImage ? this.props.post.json_metadata.content.data[1] : this.props.post.json_metadata.content.data[1]
+		let data = containsImage ? this.props.post.json_metadata.content.data[1] : this.props.post.json_metadata.content.data[0];
 		return <div className={classes.concat([]).join(' ')}>
 			<PostUserMeta profile={{name: this.props.postingUser.json_metadata.profile.name, image: this.props.postingUser.json_metadata.profile.profile_image}}
 				created={this.props.post.created} communities={getCommunitiesForPost(this.props.post)} className={['uk-padding-remove-left'].join(' ')}/>
 			<div className={[styles.rightContent].join(' ')}>
 				{data.content}
 			</div>
+			<CustomTags post={this.props.post.json_metadata.tags} className={['uk-margin-top'].join(' ')}/>
 			<ActionBar post={this.props.post}/>
 		</div>
 	}
