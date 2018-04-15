b75ffee72d21b431ea2e635e45e2c5896d02d5ec
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-15 12:03:57 +0530

    Add single post view component

---
+++ src/components/postSingle/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import indexStyles from '../../index.scss';
+
+class PostSingle extends React.Component {
+	getLeftSection() {
+
+		if (this.props.post.json_metadata.content.data[0].type === 'image') {
+			return	<div className={['uk-width-1-1@s', 'uk-width-1-2@m', 'uk-width-1-2@l', 'uk-padding-remove'].join(' ')}>
+				<img src={this.props.post.json_metadata.content.data[0].content} style={{margin: 'auto'}} alt=''/>
+			</div>
+		}
+	}
+
+	getRightSection() {
+		let containsImage = this.props.post.json_metadata.content.data[0].type === 'image';
+		let classes = ['uk-width-1-1@s', 'uk-width-2-3@m', 'uk-width-1-3@l'];
+		containsImage && (classes = ['uk-width-1-1@s', 'uk-width-1-2@m', 'uk-width-1-2@l'])
+		let data = containsImage ? this.props.post.json_metadata.content.data[1] : this.props.post.json_metadata.content.data[1]
+		return <div className={classes.join(' ')}>
+			{data.content}
+		</div>
+	}
+
+	render() {
+		return <div uk-grid={'true'} className={[indexStyles.white].join(' ')}>
+			{this.getLeftSection()}
+			{this.getRightSection()}
+		</div>
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		post: state.allPosts.posts[ownProps.postPermlink]
+	}
+};
+
+export default connect(mapStateToProps)(PostSingle);
