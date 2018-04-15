d7ed37a15c714def611e55600bb86aa685782c7a
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-15 12:04:23 +0530

    Render post when loading single post view

--- src/components/contentSingle/index.js
+++ src/components/contentSingle/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {loadPost} from '../../actions/allPostsActions';
+import PostSingle from '../postSingle';
 
 class ContentSingle extends React.Component {
 	constructor(props) {
@@ -10,8 +11,14 @@ class ContentSingle extends React.Component {
 	}
 
 	render() {
+		if (!this.props.post) {
+			return <div className={['uk-container', 'uk-margin-top'].join(' ')}>Loading...</div>
+		}
+
 		return <div className={['uk-container', 'uk-margin-top'].join(' ')}>
-			This is full content for the post.
+			{this.props.post.json_metadata.content.type === 'post' ?
+				<PostSingle postPermlink={`${this.props.post.author}/${this.props.post.permlink}`}/> :
+				'This is an article'}
 		</div>
 	}
 }
