1db0ea354f51f7adc51cd87cd7cf7baee8fb33cc
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-16 11:28:56 +0530

    Implement cases for fetching top replies

--- src/reducers/repliesReducer.js
+++ src/reducers/repliesReducer.js
@@ -1,3 +1,7 @@
+import _ from 'lodash';
+
+import {actionTypes} from '../actions/repliesActions';
+
 const initialState = {};
 /*
 {
@@ -14,5 +18,41 @@ const initialState = {};
 */
 
 export const repliesReducer = (state = initialState, action) => {
-	return state;
+	let key = `${action.parentAuthor}/${action.parentPermlink}`;
+
+	// Make sure key exists
+	!state[key] && (state[key] = {
+		error: null,
+		loading: false,
+		replies: {}
+	});
+
+	let postReplies = _.cloneDeep(state[key]);
+
+	switch (action.type) {
+		case actionTypes.REPLIES_LOAD_INIT:
+			postReplies.error = null;
+			postReplies.loading = true;
+			state[key] = postReplies;
+			return state;
+
+		case actionTypes.REPLIES_LOAD_DONE:
+			postReplies.error = null;
+			postReplies.loading = false;
+			action.results.foreach(reply => {
+				let innerKey = `${reply.author}/${reply.permlink}`;
+				postReplies.replies[innerKey] = reply;
+			});
+			state[key] = postReplies;
+			return state;
+
+		case actionTypes.REPLIES_LOAD_ERROR:
+			postReplies.error = action.reason;
+			postReplies.loading = false;
+			state[key] = postReplies;
+			return state;
+
+		default:
+			return state;
+	}
 }
