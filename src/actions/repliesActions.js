be0e1ce273fda633a5f52e905ed44276024cabc1
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-17 12:21:28 +0530

    Add action for adding reply

--- src/actions/repliesActions.js
+++ src/actions/repliesActions.js
@@ -15,3 +15,12 @@ export const loadReplies = (parentAuthor, parentPermlink) => dispatch => {
 		.then(results => dispatch({type: actionTypes.REPLIES_LOAD_DONE, parentAuthor, parentPermlink, results}))
 		.catch(reason => dispatch({type: actionTypes.REPLIES_LOAD_ERROR, parentAuthor, parentPermlink, reason}));
 }
+
+export const addReply = (parentAuthor, parentPermlink, body) => dispatch => {
+	dispatch({type: actionTypes.ADD_REPLY_INIT, parentAuthor, parentPermlink, body});
+	steemAPI.addReply(parentAuthor, parentPermlink, body)
+		.then(result => {
+			dispatch({type: actionTypes.ADD_REPLY_DONE, parentAuthor, parentPermlink, body, result});
+		})
+		.catch(reason => dispatch({type: actionTypes.ADD_REPLY_ERROR, parentAuthor, parentPermlink, body, reason}));
+}
