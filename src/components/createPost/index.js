223c272323bd5968fdfb68507e0d35f0104c68bd
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-22 15:13:42 +0530

    Add post media uploader component in post create component

--- src/components/createPost/index.js
+++ src/components/createPost/index.js
@@ -11,6 +11,7 @@ import {
 	changeCommunity, changeMedia, clearError, createPost, setHashtags,
 	postCreateError, removeMedia, resetPostCreate,
 } from "../../actions/createPostActions";
+import PostMediaUpload from '../postMediaUpload';
 
 class CreatePost extends React.Component {
 
@@ -133,42 +134,6 @@ class CreatePost extends React.Component {
 		</div>
 	}
 
-	getMediaUploader() {
-		return <div className={['uk-margin-top', styles.mediaUpload].join(' ')}>
-			<div uk-grid="true" className={'uk-margin-remove'}>
-				<div className={[styles.upload].join(' ')} onClick={this.handleUploadImageClick.bind(this)}>
-					<span className={[indexStyles.marginRightSmall].join(' ')}>
-						<svg fill="#444" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-							<path
-								d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/>
-							<path d="M0 0h24v24H0z" fill="none"/>
-						</svg>
-					</span>
-					<span>Image</span>
-				</div>
-				{/* <div className={[styles.upload].join(' ')}>
-				<span className={[indexStyles.marginRightSmall].join(' ')}>
-					<svg fill="#444" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-						<path d="M0 0h24v24H0z" fill="none"/>
-						<path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
-					</svg>
-				</span>
-					<span>Music</span>
-				</div> */}
-				<div className={[styles.upload].join(' ')}>
-				<span className={[indexStyles.marginRightSmall].join(' ')}>
-					<svg fill="#444" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-						<path d="M0 0h24v24H0z" fill="none"/>
-						<path
-							d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4z"/>
-					</svg>
-				</span>
-					<span>Video</span>
-				</div>
-			</div>
-		</div>
-	}
-
 	getMediaViewer() {
 		switch (this.props.media.type) {
 			case 'image':
@@ -225,7 +190,7 @@ class CreatePost extends React.Component {
 				<div className={['uk-margin-top', indexStyles.separator].join(' ')}/>
 				{this.getCommunitySelector()}
 				<div className={['uk-margin-top', indexStyles.separator].join(' ')}/>
-				{this.getMediaUploader()}
+				<PostMediaUpload/>
 				{this.getErrors()}
 			</div>
 		</div>
