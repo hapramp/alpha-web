96b4fd55184f0f7406e586293c34da32692d5c7f
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-24 12:16:56 +0530

    Remove hapramp vote logic from reducer

--- src/reducers/allPostsReducer.js
+++ src/reducers/allPostsReducer.js
@@ -28,26 +28,6 @@ export const allPostsReducer = (state = initialState, action) => {
 			} else {
 				posts[key].active_votes.push({voter: currentUser, percent: action.vote * 20 * 100});
 			}
-			// current_vote = posts[key].hapramp_cu_vote;
-			// if (action.vote === 0) {  // Remove vote
-			// 	posts[key].hapramp_cu_vote = null;
-			// 	current_vote && posts[key].net_votes--;
-			// 	current_vote && (posts[key].hapramp_rating = (posts[key].hapramp_rating * posts[key].hapramp_votes - current_vote)
-			// 		/ (posts[key].hapramp_votes - 1));
-			// 	current_vote && posts[key].hapramp_votes--;
-			// 	!posts[key].hapramp_votes && (posts[key].hapramp_rating = 0.0);  // Earlier divided by zero, prevent NaN
-			// } else if (posts[key].hapramp_cu_vote == null) {  // Not voted earlier
-			// 	posts[key].hapramp_cu_vote = action.vote;
-			// 	posts[key].net_votes++;
-			// 	posts[key].hapramp_rating = (posts[key].hapramp_rating * posts[key].hapramp_votes + action.vote)
-			// 		/ (posts[key].hapramp_votes + 1);
-			// 	posts[key].hapramp_votes++;
-			// } else {  // Updating vote
-			// 	let vote_to_deduct = posts[key].hapramp_cu_vote;
-			// 	posts[key].hapramp_cu_vote = action.vote;
-			// 	posts[key].hapramp_rating = (posts[key].hapramp_rating * posts[key].hapramp_votes + action.vote - vote_to_deduct)
-			// 		/ (posts[key].hapramp_votes);
-			// }
 			return {...state, posts};
 
 		// TODO: Handle post upvote error case
