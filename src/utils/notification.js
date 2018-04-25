72ded12952e1a012c8ffe07fd2c31870d3839297
Pratyush Singh <singh.pratyush96@gmail.com>
2018-04-25 17:15:27 +0530

    Fix repeat notifications

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -5,9 +5,21 @@ const defaultOptions = {
 	timeout: 4000,
 }
 
+const messages = {};
+
+const notify = (message, options, repeat) => repeat ? UIKit.notification(message, options) : notifyWithoutRepeat(message, options);
+
+const notifyWithoutRepeat = (message, options) => {
+	!messages[message] && UIKit.notification(message, options);
+	messages[message] = true;
+	setTimeout(() => delete messages[message], options.timeout);
+};
+
+let options;
+
 export default {
-	info: message => UIKit.notification(message, {...defaultOptions, status: 'primary'}),
-	success: message => UIKit.notification(message, {...defaultOptions, status: 'success'}),
-	danger: message => UIKit.notification(message, {...defaultOptions, status: 'danger', timeout: 5000}),
-	warning: message => UIKit.notification(message, {...defaultOptions, status: 'warning'}),
-}
+	info: (message, repeat = false) => notify(message, {...defaultOptions, status: 'primary'}, repeat),
+	success: (message, repeat = false) => notify(message, {...defaultOptions, status: 'success'}, repeat),
+	danger: (message, repeat = false) => notify(message, {...defaultOptions, status: 'danger', timeout: 5000}, repeat),
+	info: (message, repeat = false) => notify(message, {...defaultOptions, status: 'warning'}, repeat),
+};
