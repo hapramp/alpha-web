7a4e6c3040718c92e046472cd7131de9b8ffa93e
Pratyush Singh <singh.pratyush96@gmail.com>
2018-05-10 23:24:20 +0530

    Fix logo quality at home

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import styles from './styles.scss';
-import logo from '../header/logo.png';
+import logo from './logo.png';
 import {login} from "../../actions/loginActions";
 
 class SignIn extends React.Component {
