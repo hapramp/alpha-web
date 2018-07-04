2e78413af3af6556657735607f0bb6b7ef1e7be5
Pratyush Singh <singh.pratyush96@gmail.com>
2018-07-04 16:10:38 +0530

    Add redux-auth-wrapper for auth required routes

--- src/utils/decorators.js
+++ src/utils/decorators.js
@@ -1,3 +1,5 @@
+import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect';
+
 import notify from './notification';
 
 export const notAuthorizedErrorAction = () => () => {
@@ -6,3 +8,10 @@ export const notAuthorizedErrorAction = () => () => {
 };
 
 export const authRequired = target => (localStorage.getItem('username') ? target : notAuthorizedErrorAction);
+
+/** MORE: https://mjrussell.github.io/redux-auth-wrapper/docs/Getting-Started/ReactRouter4.html */
+export const authRequiredComponent = connectedRouterRedirect({
+  redirectPath: '/',
+  authenticatedSelector: state => state.login.isLoggedIn,
+  wrapperDisplayName: 'AuthRequiredComponent',
+});
