efdc56d13f561590eadb4c6a4148451558ca43c1
Pratyush Singh <singh.pratyush96@gmail.com>
2018-10-08 12:16:04 +0530

    Fix invalid cookie expiry date

--- src/components/OAuthCallback/index.js
+++ src/components/OAuthCallback/index.js
@@ -11,7 +11,7 @@ const OAuthCallback = (props) => {
   const params = new URLSearchParams(props.location.search);
   const accessToken = params.get('access_token');
   const username = params.get('username');
-  const expiresIn = parseInt(params.get('expires_in'), 10);
+  const expiresIn = parseInt(params.get('expires_in'), 10) / 60 / 60 / 24; // Convert seconds to days
   Cookie.set('access_token', accessToken, { expires: expiresIn });
   Cookie.set('username', username, { expires: expiresIn });
   steemAPI.sc2Api.setAccessToken(accessToken);
