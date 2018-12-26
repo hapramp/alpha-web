8d714ea085bec48a115d4bdb30e2eecbdee8c65f
Pratyush Singh <singh.pratyush96@gmail.com>
2018-12-26 23:49:13 +0530

    Add external link widget

---
+++ src/components/ExternalLinksWidget/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import styles from './styles.scss';
+
+const links = [
+  {
+    text: '1Ramp.io',
+    link: 'https://1ramp.io',
+  },
+  {
+    text: 'About',
+    link: 'https://1ramp.io/about.html',
+  },
+  {
+    text: 'Help',
+    link: 'https://1ramp.io/faq.html',
+  },
+  {
+    text: '#WeAreOne',
+    link: 'https://1ramp.io/weareone.html',
+  },
+  {
+    text: 'Discord',
+    link: 'https://discordapp.com/invite/c6N9kVM',
+  },
+  {
+    text: 'Steemit',
+    link: 'https://steemit.com/@the1ramp',
+  },
+  {
+    text: 'GitHub',
+    link: 'https://github.com/hapramp',
+  },
+  {
+    text: 'Android App',
+    link: 'https://play.google.com/store/apps/details?id=com.hapramp',
+  },
+  {
+    text: 'Terms',
+    link: 'https://1ramp.io/terms.html',
+  },
+];
+
+export default () => (
+  <div className={styles.container}>
+    {
+      links.map(link => (
+        <a target="_blank" href={link.link}>{link.text}</a>
+      ))
+    }
+  </div>
+);
