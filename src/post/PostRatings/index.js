359e1241d91142050a08fa68365a3e8fcc81b548
Pratyush Singh <singh.pratyush96@gmail.com>
2019-01-22 18:26:35 +0530

    Add comments explaining rating modal behaviour

--- src/post/PostRatings/index.js
+++ src/post/PostRatings/index.js
@@ -10,6 +10,7 @@ import RatingCard from './RatingCard';
 import styles from './styles.scss';
 
 const itemsPerPage = 20;
+// Show modal at app root
 const parentSelector = () => document.getElementById('root');
 const preventParentScroll = () => {
   document.body.style.overflow = 'hidden';
@@ -22,14 +23,33 @@ const noOp = () => { };
 const PostRatings = ({
   ratings, showRatings, onClose, postValue,
 }) => {
+  // Sort - desc rshares
   const sortedRatings = ratings.sort((ratingA, ratingB) => ratingB.rshares - ratingA.rshares);
   let totalRshares = 1;
-  if (showRatings) {
+  if (showRatings) { // Do not calculate total rshares if rating is not shown
     totalRshares = sortedRatings.reduce((acc, curr) => acc + parseInt(curr.rshares, 10), 0);
   }
+  // Will be used in children components to display vote value
   const ratio = postValue / totalRshares;
+
+  /**
+   * If a post contains way too many ratings, it takes long
+   * before all of them are rendered in the DOM. To prevent
+   * this, we paginate the ratings with a page size of
+   * 'itemsPerPage'.
+   *
+   * 'displayCount' keeps track of the number of ratings
+   * to be rendered. It is set initially to 0 so that no
+   * ratings are rendered when the overlay is inactive
+   */
   const [displayCount, changeDisplayCount] = useState(0);
   return (
+    /**
+     * The document starts scrolling once the modal is
+     * scrolled to bottom. To prevent this, disable document
+     * scrolling once the modal is active and enable
+     * it again when done
+     */
     <Modal
       isOpen={showRatings}
       onAfterOpen={preventParentScroll}
@@ -59,13 +79,14 @@ const PostRatings = ({
             pageStart={0}
             loadMore={() => changeDisplayCount(displayCount + itemsPerPage)}
             hasMore={displayCount < sortedRatings.length}
+            // Can't use window as it will result in rendering everything at once
             useWindow={false}
             className={styles.ratingListContainer}
           >
             {
               take(sortedRatings, displayCount).map(rating => (
                 <RatingCard
-                  ratio={ratio}
+                  ratio={ratio} // To calculate vote value
                   ratingObject={rating}
                   key={rating.voter}
                 />
