e1e1c1aa1bfebfff6c565567939f70adc7cc6077
Pratyush Singh <singh.pratyush96@gmail.com>
2019-01-22 18:04:38 +0530

    Add feature to upload image in markdown editor

--- src/post/create/CreateArticle/MarkdownEditor/index.js
+++ src/post/create/CreateArticle/MarkdownEditor/index.js
@@ -4,35 +4,79 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AutosizeTextarea from 'react-autosize-textarea';
 
+import MediaSelector from '../../CreatePost/MediaSelector';
 import PostBody from '../../../PostBody';
 import styles from './styles.scss';
 import { getMarkdownText } from '../reducer';
-import { setMarkdownText } from '../actions';
+import { setMarkdownText, uploadImage } from '../actions';
 
-const MarkdownEditor = ({ bodyMarkdown, onChange }) => (
-  <div className={`uk-margin-top ${styles.editorWrite}`}>
-    <AutosizeTextarea
-      value={bodyMarkdown}
-      onChange={event => onChange(event.target.value)}
-      className="uk-textarea uk-margin-bottom"
-      rows={5}
-    />
-    <div className={styles.previewContainer}>
-      <span>Preview</span>
-      <PostBody className={styles.previewBody} body={bodyMarkdown} />
-    </div>
-  </div>
-);
+class MarkdownEditor extends React.Component {
+  static propTypes = {
+    bodyMarkdown: PropTypes.string,
+    onChange: PropTypes.func,
+    onImageUpload: PropTypes.func,
+  };
 
-MarkdownEditor.propTypes = {
-  bodyMarkdown: PropTypes.string,
-  onChange: PropTypes.func,
-};
+  static defaultProps = {
+    bodyMarkdown: '',
+    onChange: '',
+    onImageUpload: () => { },
+  };
 
-MarkdownEditor.defaultProps = {
-  bodyMarkdown: '',
-  onChange: '',
-};
+  constructor(props) {
+    super(props);
+
+    this.textAreaRef = null;
+    this.state = {
+      isImageUploading: false,
+    };
+
+    this.onImageSelected = this.onImageSelected.bind(this);
+  }
+
+  onImageSelected(file) {
+    if (!this.textAreaRef) {
+      return;
+    }
+    const { onImageUpload, onChange, bodyMarkdown } = this.props;
+    this.setState({ ...this.state, isImageUploading: true });
+    onImageUpload(file)
+      .then(({ url, error }) => {
+        if (error) {
+          return;
+        }
+        const startPos = this.textAreaRef.selectionStart;
+        const endPos = this.textAreaRef.selectionEnd;
+        const imageText = `![](${url})\n`;
+        onChange(`${bodyMarkdown.substring(0, startPos)}${imageText}${bodyMarkdown.substring(endPos, bodyMarkdown.length)}`);
+        this.setState({ ...this.state, isImageUploading: false });
+      });
+  }
+
+  render() {
+    const { bodyMarkdown, onChange } = this.props;
+    const { isImageUploading } = this.state;
+    return (
+      <div className={`uk-margin-top ${styles.editorWrite}`}>
+        <AutosizeTextarea
+          value={bodyMarkdown}
+          onChange={event => onChange(event.target.value)}
+          className="uk-textarea uk-margin-bottom"
+          rows={5}
+          innerRef={(ref) => { this.textAreaRef = ref; }}
+        />
+        <div className={styles.imageUploadContainer}>
+          <MediaSelector changeMedia={this.onImageSelected} />
+          { isImageUploading && <span className={styles.uploadingText}>Uploading image...</span>}
+        </div>
+        <div className={styles.previewContainer}>
+          <span>Preview</span>
+          <PostBody className={styles.previewBody} body={bodyMarkdown} />
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   bodyMarkdown: getMarkdownText(state),
@@ -40,6 +84,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   onChange: setMarkdownText,
+  onImageUpload: uploadImage,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(MarkdownEditor);
