874a4b2a7d714dbddac9ff9730f35173628ae9d2
ansarimofid <ansarimofid@gmail.com>
2019-01-30 17:39:03 +0530

    Updates onboarding content

--- src/onboard/pages/OnePlaceForAll.js
+++ src/onboard/pages/OnePlaceForAll.js
@@ -6,11 +6,11 @@ import styles from './styles.scss';
 export default () => (
   <div className={styles.oneRampIsYou}>
     <div className={styles.left}>
-      <img src={leftImage} alt="1 Place for all" />
+      <img src={leftImage} alt="Join Communities" />
     </div>
     <div className={styles.right}>
       <div className={styles.title}>
-        1Ramp is 1 Place for all
+        Join Communities
       </div>
       <div className={styles.body}>
         {'Join communities of your interest, discover exciting content, and see what happens when 8 creative domains engage on 1 platform.'}
