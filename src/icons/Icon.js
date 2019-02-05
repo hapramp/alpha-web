0e7edfebec410a13fe4f71a36daf33a9b21ecb0d
Pratyush Singh <singh.pratyush96@gmail.com>
2019-02-05 11:38:35 +0530

    Add icons for leaderboard

--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 import { getIcon } from '.';
 
-const Icon = ({ name, type, ...props }) => {
+const Icon = ({
+  name, type, format, ...props
+}) => {
   // Type must be solid or outline
   const realType = ['solid', 'outline'].find(i => type === i) ? type : 'outline';
   return (
     <img
-      src={getIcon(name, realType)}
+      src={getIcon(name, realType, format)}
       alt={name}
       {...props}
     />
@@ -18,10 +20,12 @@ const Icon = ({ name, type, ...props }) => {
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string,
+  format: PropTypes.string,
 };
 
 Icon.defaultProps = {
   type: 'outline',
+  format: 'svg',
 };
 
 export default Icon;
