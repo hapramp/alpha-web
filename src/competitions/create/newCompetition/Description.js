22a55073d4184d4e4f264300a0cf93bf250f7b38
Pratyush Singh <singh.pratyush96@gmail.com>
2019-02-13 00:16:45 +0530

    Connect new competition description to redux

--- src/competitions/create/newCompetition/Description.js
+++ src/competitions/create/newCompetition/Description.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import PrimaryButton from '../../../components/buttons/PrimaryButton';
 import ViewContainer from '../../../components/ViewContainer';
 import Input from '../../../components/input/Input';
 import TextArea from '../../../components/input/TextArea';
 import styles from './styles.scss';
+import { getNewCompetitionField } from './reducer';
+import { changeField } from './actions';
 
-const NewCompetitionDescription = () => (
+const NewCompetitionDescription = ({
+  name, description, rules,
+  setName, setDescription, setRules,
+}) => (
   <ViewContainer>
     <div>
       <Input
         placeholder="Competition Name"
         className="uk-form-large uk-margin-large-bottom"
+        value={name}
+        onChange={setName}
       />
       <TextArea
         placeholder="Description"
         className="uk-margin-small-bottom"
         rows={2}
+        value={description}
+        onChange={setDescription}
       />
       <TextArea
         placeholder="Rules"
         rows={2}
+        value={rules}
+        onChange={setRules}
       />
     </div>
     <div className={styles.bottomBar}>
@@ -34,4 +48,25 @@ const NewCompetitionDescription = () => (
   </ViewContainer>
 );
 
-export default NewCompetitionDescription;
+NewCompetitionDescription.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  rules: PropTypes.string.isRequired,
+  setName: PropTypes.func.isRequired,
+  setDescription: PropTypes.func.isRequired,
+  setRules: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  name: getNewCompetitionField(state, 'name'),
+  description: getNewCompetitionField(state, 'description'),
+  rules: getNewCompetitionField(state, 'rules'),
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  setName: event => changeField('name', event.target.value),
+  setDescription: event => changeField('description', event.target.value),
+  setRules: event => changeField('rules', event.target.value),
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCompetitionDescription);
---
+++ src/competitions/create/newCompetition/reducer.js
@@ -0,0 +1,43 @@
+import { actionTypes } from './actions';
+import { getNewCompetitionState } from '../reducer';
+
+const initialState = {
+  name: '',
+  description: '',
+  rules: '',
+  tags: [],
+  interests: [],
+  startDate: {
+    date: '',
+    time: '',
+  },
+  endDate: {
+    date: '',
+    time: '',
+  },
+  prizes: [
+    '',
+    '',
+    '',
+  ],
+  image: null,
+};
+
+const newCompetitionReducer = (state = initialState, action) => {
+  const { field, value } = action;
+  let newState;
+  switch (action.type) {
+    case actionTypes.changeField:
+      newState = { ...state };
+      newState[field] = value;
+      return newState;
+
+    default:
+      return state;
+  }
+};
+
+export default newCompetitionReducer;
+
+// Selector
+export const getNewCompetitionField = (state, field) => getNewCompetitionState(state)[field];
