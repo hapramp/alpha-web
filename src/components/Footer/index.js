925a006ddeaa8d5ba53b50c4b9768e2f7f332cc1
Pratyush Singh <singh.pratyush96@gmail.com>
2019-12-03 09:30:34 +0530

    Remove copyright text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,12 +6,9 @@ const externalLinkProps = {
 };
 
 const Footer = () => (
-  <div className="uk-background-muted uk-padding-small uk-panel uk-flex uk-flex-between">
-    <p className="uk-text-small uk-text-meta uk-margin-remove uk-marign-right">
+  <div className="uk-flex">
+    <p className="uk-text-small uk-text-meta uk-padding-small uk-margin-remove uk-background-muted">
       <a href="https://getgosocial.app/" {...externalLinkProps}>GoSocial</a> | <a href="https://hapramp.com/asteria" {...externalLinkProps}>Asteria</a> | <a href="https://1ramp.io/" {...externalLinkProps}>1Ramp</a> from <a href="https://hapramp.com/" {...externalLinkProps}>Hapramp</a>.
-    </p>
-    <p className="uk-text-small uk-text-meta uk-margin-remove">
-      &copy; Hapramp Studio Private Limited
     </p>
   </div>
 );
