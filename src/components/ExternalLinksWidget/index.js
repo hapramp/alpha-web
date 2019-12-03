b990c7f92b49fc3e25c9d18ef199f3521702d2f2
Pratyush Singh <singh.pratyush96@gmail.com>
2019-12-03 08:46:06 +0530

    Remove broken links from external widget

--- src/components/ExternalLinksWidget/index.js
+++ src/components/ExternalLinksWidget/index.js
@@ -9,16 +9,16 @@ const links = [
   },
   {
     text: 'About',
-    link: 'https://1ramp.io/about.html',
+    link: 'https://1ramp.io/about',
   },
-  {
-    text: 'Help',
-    link: 'https://1ramp.io/faq.html',
-  },
-  {
-    text: '#WeAreOne',
-    link: 'https://1ramp.io/weareone.html',
-  },
+  // {
+  //   text: 'Help',
+  //   link: 'https://1ramp.io/faq.html',
+  // },
+  // {
+  //   text: '#WeAreOne',
+  //   link: 'https://1ramp.io/weareone.html',
+  // },
   {
     text: 'Discord',
     link: 'https://discordapp.com/invite/c6N9kVM',
@@ -35,10 +35,10 @@ const links = [
     text: 'Android App',
     link: 'https://play.google.com/store/apps/details?id=com.hapramp',
   },
-  {
-    text: 'Terms',
-    link: 'https://1ramp.io/terms.html',
-  },
+  // {
+  //   text: 'Terms',
+  //   link: 'https://1ramp.io/terms.html',
+  // },
 ];
 
 export default () => (
